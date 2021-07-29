4bbd5c502716113d769b00fb2bc810ef84b0c522
viprabhu9401 <vinu.prabhu@gmail.com>
2021-07-29 18:38:12 -0500

    intial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+import flask
+import pandas as pd
+import pickle
+from flask import Flask,render_template, request
+
+app = Flask(__name__)
+## loading the model
+pickle_in = open('xgcars.pkl','rb')
+predictor = pickle.load(pickle_in)
+
+## loading the make encoder
+make_pickle_in = open('make.pkl','rb')
+make_transformer = pickle.load(make_pickle_in)
+
+## loading the make encoder
+model_pickle_in = open('model.pkl','rb')
+model_transformer = pickle.load(model_pickle_in)
+
+## loading the fuel encoder
+fuel_pickle_in = open('fuel.pkl','rb')
+fuel_transformer = pickle.load(fuel_pickle_in)
+
+## loading the transmission encoder
+trans_pickle_in = open('trans.pkl','rb')
+trans_transformer = pickle.load(trans_pickle_in)
+
+@app.route('/')
+def hello():
+    return 'welcome!'
+
+@app.route('/predict')
+def predict_price():
+    web_make = request.args.get('make')
+    mke = int(make_transformer.transform([web_make])[0])
+
+    web_model = request.args.get('model')
+    mdel = int(model_transformer.transform([web_model])[0])
+
+    yr = int(request.args.get('year'))
+
+    web_trans = request.args.get('transmission')
+    trans = int(trans_transformer.transform([web_trans])[0])
+
+    miles = int(request.args.get('mileage'))
+
+    web_fuel = request.args.get('fuelType')
+    fuel = int(fuel_transformer.transform([web_fuel])[0])
+
+    engine=float(request.args.get('engineSize'))
+
+    tx = float(request.args.get('tax'))
+
+    mpgal = float(request.args.get('mpg'))
+
+    data = {'year':yr,'transmission':1,'mileage':miles,'fuelType':fuel,'tax':tx,
+                               'mpg':mpgal,'engineSize':engine,'make':mke,'model':mdel}
+    test_df = pd.DataFrame(data,index=[0])
+    predicted_price = predictor.predict(test_df)[0]
+    return "the predicted car price is " + str(predicted_price)
+
+@app.route('/content')
+def content():
+    return 'The content page for the application - first flast app code'
+
+@app.route('/aboutpage')
+def about():
+    return 'about page content'
+
+@app.route('/htmpage1')
+def htmpage1():
+    return '<h1>This is the firt html output line from a python app</h1>'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+        
+
+
+if __name__=='__main__':
+    app.run(debug = True)
